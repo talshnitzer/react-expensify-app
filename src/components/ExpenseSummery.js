810eb93bbb2b8c173d92da68242d95e23795c27c
Backend <backend@Tals-MacBook-Air.local>
2019-08-15 15:28:00 +0300

    fix according Andrew sollution

--- src/components/ExpenseSummery.js
+++ src/components/ExpenseSummery.js
@@ -4,15 +4,38 @@ import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/selectExpensesTotal';
 import numeral from 'numeral';
 
-export const ExpenseSummery = (props) => (
-    <div>
-        <p>Viewing {props.expenses.length} expenses totalling {numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00')}</p>
-    </div>
-);
+//Tal's solution
+// export const ExpenseSummery = (props) => (
+//     <div>
+//         <p>Viewing {props.expenses.length} expenses totalling {numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00')}</p>
+//     </div>
+// );
 
+//Andrew's solution
+export const ExpenseSummery = ({expenseCount, ExpensesTotal}) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpenseTotal = numeral(ExpensesTotal / 100).format('$0,0.00');
+
+    return (
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpenseTotal}</h1>
+        </div>
+    );
+};
+
+//Tal's sollution
+// const mapStateToProps = (state) => {
+//     return{
+//         expenses: selectExpenses(state.expenses, state.filters)
+//     };
+// };
+
+//Andrew's solution
 const mapStateToProps = (state) => {
+    const visibleExpenses =selectExpenses(state.expenses, state.filters);
     return{
-        expenses: selectExpenses(state.expenses, state.filters)
+        expenseCount: visibleExpenses.length,
+        ExpensesTotal: selectExpensesTotal(visibleExpenses)
     };
 };
 
--- src/tests/components/ExpenseSummery.test.js
+++ src/tests/components/ExpenseSummery.test.js
@@ -3,13 +3,27 @@ import { shallow } from 'enzyme';
 import { ExpenseSummery } from '../../components/ExpenseSummery';
 import expenses from '../fixtures/expenses';
 
+//Tal's solution
+// test('should render ExpenseSummery correctly with 1 expense' , () => {
+//     const wrapper = shallow(<ExpenseSummery expense={[expenses[0]]}/>);
+//     expect(wrapper).toMatchSnapshot();
+// });
+
+//Andrew's solution
 test('should render ExpenseSummery correctly with 1 expense' , () => {
-    const wrapper = shallow(<ExpenseSummery expenses={[expenses[0]]}/>);
+    const wrapper = shallow(<ExpenseSummery expenseCount={1} ExpensesTotal={235}/>);
     expect(wrapper).toMatchSnapshot();
 });
 
-test('should render ExpenseSummery correctly with 2 expenses' , () => {
-    const wrapper = shallow(<ExpenseSummery expenses={[expenses[0], expenses[1]]}/>);
+//Tal's version
+// test('should render ExpenseSummery correctly with 2 expenses' , () => {
+//     const wrapper = shallow(<ExpenseSummery expenses={[expenses[0], expenses[1]]}/>);
+//     expect(wrapper).toMatchSnapshot();
+// });
+
+//Andrew's solution
+test('should render ExpenseSummery correctly with multiple expenses' , () => {
+    const wrapper = shallow(<ExpenseSummery expenseCount={23} ExpensesTotal={23512346890}/>);
     expect(wrapper).toMatchSnapshot();
 });
 
